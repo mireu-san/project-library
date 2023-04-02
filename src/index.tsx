ca3edf500a6b037bfbd239d74152c7d237642651
Chesed Kim <starmireu@gmail.com>
2023-04-02 16:19:53 +0900

    temp

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { RecoilRoot } from 'recoil';
+// Start Applying RecoilRoot once first draft is complete!
+// import { RecoilRoot } from 'recoil';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<RecoilRoot>
-			<App />
-		</RecoilRoot>
+		{/* <RecoilRoot> */}
+		{/* <Routes> */}
+		<App />
+		{/* </Routes> */}
+		{/* </RecoilRoot> */}
 	</React.StrictMode>
 );
