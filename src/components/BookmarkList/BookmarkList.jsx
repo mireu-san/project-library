a0d4035dac57cfb2dd4a8f394a85d0a068309eb5
Jongwan Kim <starmireu@gmail.com>
2023-04-04 17:54:38 +0900

    backup

--- src/components/BookmarkList/BookmarkList.jsx
+++ src/components/BookmarkList/BookmarkList.jsx
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Book from '../BookList/Book';
-import { useSpring } from 'react-spring';
 import './BookmarkList.css';
 
 const BookmarkList = () => {
 	const [bookmarks, setBookmarks] = useState([]);
-	const styles = useSpring({
-		transform: 'scale(1)', // initial scale
-		from: { transform: 'scale(0)' }, // animate from scale 0
-		config: { tension: 150, friction: 10 }, // animation config
-	});
 
 	useEffect(() => {
 		const bookmarkKeys = Object.keys(localStorage);
@@ -34,23 +28,21 @@ const BookmarkList = () => {
 	};
 
 	return (
-		<form style={styles}>
-			<div className="root-style-bookmark">
-				<h2>Bookmarked Items:</h2>
-				{bookmarks.length === 0 ? (
-					<p>No bookmarks yet.</p>
-				) : (
-					bookmarks.map((book) => (
-						<div key={book.id}>
-							<Book {...book} />
-							<button onClick={() => handleRemoveBookmark(book.id)}>
-								Remove Bookmark
-							</button>
-						</div>
-					))
-				)}
-			</div>
-		</form>
+		<div className="root-style-bookmark">
+			<h2>Bookmarked Items:</h2>
+			{bookmarks.length === 0 ? (
+				<p>No bookmarks yet.</p>
+			) : (
+				bookmarks.map((book) => (
+					<div key={book.id}>
+						<Book {...book} />
+						<button onClick={() => handleRemoveBookmark(book.id)}>
+							Remove Bookmark
+						</button>
+					</div>
+				))
+			)}
+		</div>
 	);
 };
 
