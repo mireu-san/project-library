fd64bd7928301cd10ea108045de573b6c4c34fed
Jongwan Kim <starmireu@gmail.com>
2023-04-06 14:51:20 +0900

    work in progress - css, bookmarkList

--- src/components/BookmarkList/BookmarkList.jsx
+++ src/components/BookmarkList/BookmarkList.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import Book from '../BookList/Book';
+// import Book from '../BookList/Book';
 import './BookmarkList.css';
 
 const BookmarkList = () => {
 	const [bookmarks, setBookmarks] = useState([]);
+	const [selectedBooks, setSelectedBooks] = useState([]);
 
 	useEffect(() => {
 		const bookmarkKeys = Object.keys(localStorage);
 		if (bookmarkKeys.length === 0) {
-			// call setBookmarks with blank array to empty default bookmark section.
 			setBookmarks([]);
 		} else {
 			const selectedBooks = bookmarkKeys.filter(
@@ -27,17 +27,76 @@ const BookmarkList = () => {
 		);
 	};
 
+	const handleBookSelect = (bookId) => {
+		setSelectedBooks([...selectedBooks, bookId]);
+	};
+
+	const handleBookDeselect = (bookId) => {
+		setSelectedBooks(selectedBooks.filter((id) => id !== bookId));
+	};
+
+	const handleDownloadFile = () => {
+		const fileContents = selectedBooks
+			.map((bookId) => {
+				const book = bookmarks.find((b) => b.id === bookId);
+				return book.title;
+			})
+			.join('\n');
+
+		// Download all checked books/book as txt file
+		const blob = new Blob([fileContents], { type: 'text/plain' });
+		const url = URL.createObjectURL(blob);
+		const link = document.createElement('a');
+		link.download = 'bookmarks.txt';
+		link.href = url;
+		link.click();
+
+		// Once download file is clicked, clear the checkbox selection
+		setSelectedBooks([]);
+	};
+
 	return (
 		<div className="root-style-bookmark">
 			<h2>Bookmarked Items:</h2>
+			<div className="download-button-container">
+				<button
+					onClick={handleDownloadFile}
+					disabled={selectedBooks.length === 0}
+					className="download-button"
+				>
+					Download File
+				</button>
+			</div>
 			{bookmarks.length === 0 ? (
-				<p>No bookmarks yet.</p>
+				<p className="no-bookmark-message">No bookmarks yet.</p>
 			) : (
 				bookmarks.map((book) => (
-					<div key={book.id}>
-						{/* this spread operator passes all properties from Book.jsx */}
-						<Book {...book} />
-						<button onClick={() => handleRemoveBookmark(book.id)}>
+					<div key={book.id} className="bookmark-container">
+						<label className="book-label">
+							<input
+								type="checkbox"
+								checked={selectedBooks.includes(book.id)}
+								onChange={(e) =>
+									e.target.checked
+										? handleBookSelect(book.id)
+										: handleBookDeselect(book.id)
+								}
+								className="book-checkbox"
+							/>
+							<img src={book.cover_img} alt="cover" className="book-image" />
+							<div className="book-details">
+								<span className="book-title">{book.title}</span>
+								<span className="book-author">{book.author}</span>
+								<span className="book-edition-count">{book.edition_count}</span>
+								<span className="book-first-publish-year">
+									{book.first_publish_year}
+								</span>
+							</div>
+						</label>
+						<button
+							onClick={() => handleRemoveBookmark(book.id)}
+							className="remove-bookmark-button"
+						>
 							Remove Bookmark
 						</button>
 					</div>
